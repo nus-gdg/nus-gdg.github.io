3b6d64d7e19b571da537b743d2f054e9471c237a
Bacon-Strips <86605835+Bacon-Strips@users.noreply.github.com>
2024-06-22 18:11:58 +0800

    Stylize Main Event Card

--- new/src/sections/events/mainEventItem.tsx
+++ new/src/sections/events/mainEventItem.tsx
@@ -1,4 +1,5 @@
 import useCountdown from "../../hooks/useCountdown";
+import HourGlass from "../../assets/event-icons/hourglass.svg";
 
 interface Props {
   name: string;
@@ -18,15 +19,17 @@ const MainEventItem = ({ name, details, link, mainIcon, sideIcon, date }: Props)
       <div className="main-event-card-header">
         <span className="main-event-tagline">{tagline}</span>
         <span className="main-event-name">{name}</span>
-        <span className="main-event-countdown">
-          <b>{days}</b> DAYS<b> {hours} </b> HOURS <b>{minutes}</b> MINUTES
-        </span>
-      </div>
-      <div className="main-event-card-body">
+        <div className="main-event-countdown">
+          <img src={HourGlass} />
+          <span className="main-event-countdown-text">
+            <b>{days}</b> DAYS<b> {hours} </b> HOURS <b>{minutes}</b> MINUTES
+          </span>
+        </div>
         <a href={link}>
-          <button className="main-event-link">Official Website→</button>
+          <button className="main-event-button">Official Website→</button>
         </a>
       </div>
+      <div className="main-event-card-body"></div>
     </div>
   );
 };
