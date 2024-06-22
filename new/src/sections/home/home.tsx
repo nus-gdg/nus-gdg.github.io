6123818478f896e6cbfa8a387cfc726e75553147
Nguyen <87511888+nknguyenhc@users.noreply.github.com>
2024-06-22 22:36:27 +0800

    Landing banner (#16)
    ---------
    
    Co-authored-by: Singa-pirate <E0969244@u.nus.edu>

--- new/src/sections/home/home.tsx
+++ new/src/sections/home/home.tsx
@@ -1,7 +1,30 @@
+import Button from "../../components/button/button";
+import landingImage from "../../assets/images/landing-placeholder.svg";
+
 const Home = (): JSX.Element => {
   return (
-    <div id="home">
-      <div style={{ height: "100vh" }}>Home</div> {/* Replace */}
+    <div id="home" className="home">
+      <div className="home-left">
+        <div className="home-text">
+          <div className="home-text-lg">
+            Unlock your <span className="home-text-creative">Creative </span> Potential in Game Development!
+          </div>
+          <div className="home-text-sm">
+            Turn your enthusiasm for gaming into a talent for creating compelling, immersive experiences - no prior
+            experience necessary, beginners are warmly welcomed!
+          </div>
+        </div>
+        <div className="home-learn-more">
+          <Button onClick={() => (window.location.hash = "#about")}>
+            <a href="#about" className="home-learn-more-text">
+              Learn More
+            </a>
+          </Button>
+        </div>
+        <div className="home-banner">
+          <img src={landingImage} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
