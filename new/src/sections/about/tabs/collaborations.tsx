6783c153a867cfb50e465392d5d19d58581ebf2e
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2024-07-06 11:19:03 +0800

    Add collaboration header

--- new/src/sections/about/tabs/collaborations.tsx
+++ new/src/sections/about/tabs/collaborations.tsx
@@ -3,18 +3,27 @@ import { COLLABORATIONS } from "../../../constants/collaborations";
 const Collaborations = (): JSX.Element => {
   return (
     <div className="collaboration">
-      {COLLABORATIONS.map((collaboration) => (
-        <div key={collaboration.title} className="collaboration-item">
-          <div className="collaboration-item-header">
-            <img className="collaboration-item-header-img" src={collaboration.image} alt="" />
-            <div className="collaboration-item-header-text">{collaboration.level}</div>
-          </div>
-          <div className="collaboration-item-body">
-            <div className="collaboration-item-body-title">{collaboration.title}</div>
-            <div className="collaboration-item-body-description">{collaboration.description}</div>
-          </div>
+      <div className="collaboration-header">
+        <div className="collaboration-header-title">Collaborations</div>
+        <div className="collaboration-header-description">
+          We host collaborations within the NUS GDG to get students involved in making games. Members may choose to join
+          either or both of the following pathways depending on skill level.
         </div>
-      ))}
+      </div>
+      <div className="collaboration-list">
+        {COLLABORATIONS.map((collaboration) => (
+          <div key={collaboration.title} className="collaboration-item">
+            <div className="collaboration-item-header">
+              <img className="collaboration-item-header-img" src={collaboration.image} alt="" />
+              <div className="collaboration-item-header-text">{collaboration.level}</div>
+            </div>
+            <div className="collaboration-item-body">
+              <div className="collaboration-item-body-title">{collaboration.title}</div>
+              <div className="collaboration-item-body-description">{collaboration.description}</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
