f65b750d9da3e89f6b312684308c6afd44d83114
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2025-04-01 16:45:42 +0800

    Fix typo

--- new/src/constants/timeline.tsx
+++ new/src/constants/timeline.tsx
@@ -24,7 +24,7 @@ export type WeekItem = {
     | {
         name: string;
         updatedTime?: TimeData;
-        updatedVenue?: TimeData;
+        updatedVenue?: Venue;
       }
     | NoSessionReason;
   poster?: string;
