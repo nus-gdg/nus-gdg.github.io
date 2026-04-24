e59729f9c03ff1cc2e2757b7bbc4049494e9a5c8
yk-tuturu <waikei.koh@gmail.com>
2026-04-24 22:48:51 +0800

    update some packages and update event info

--- new/src/constants/events.tsx
+++ new/src/constants/events.tsx
@@ -4,18 +4,18 @@ import EventIcon1 from "../assets/event-icons/Upcoming_Events_ProjectDev.png";
 import EventIcon2 from "../assets/event-icons/Upcoming_Events_Level1Games.png";
 
 export const MAIN_EVENT = {
-  name: "GameCraft! 2024",
-  details: ["Dec 2024", "Game Jam", "7 Days", "50+ Participants"],
+  name: "GameCraft! 2025",
+  details: ["Dec 2025", "Game Jam", "7 Days", "50+ Participants"],
   link: "https://gamecraft.nusgdg.org/",
   mainIcon: MainEventMainIcon,
   sideIcon: MainEventSideIcon,
-  date: new Date("2024-12-14"),
+  date: new Date("2025-12-14"),
 };
 
 export const EVENTS = [
   {
     name: "Project: Dev",
-    details: ["Sep 2024", "Game Project", "1 Semester", "9 Teams"],
+    details: ["Sep 2025", "Game Project", "1 Semester", "9 Teams"],
     description:
       "Show off your new game dev skills by forming a team to make a basic level 1 game!" +
       " Or you can also choose to join one of the Project: Dev teams if you’re up for a bigger challenge.",
@@ -24,7 +24,7 @@ export const EVENTS = [
   },
   {
     name: "Level 1 Games",
-    details: ["Sep 2024", "Game Project", "1 Semester", "5 Teams"],
+    details: ["Sep 2025", "Game Project", "1 Semester", "5 Teams"],
     description:
       "Show off your new game dev skills by forming a team to make a basic level 1 game!" +
       " Or you can also choose to join one of the Project: Dev teams if you’re up for a bigger challenge.",
